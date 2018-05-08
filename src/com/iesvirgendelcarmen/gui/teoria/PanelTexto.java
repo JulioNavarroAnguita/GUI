9579f83985b4313a2b8a54f7a6ed67b975fbddcf
minip <minip@192.168.1.138>
2018-05-08 09:53:47 +0200

    Trabajando con JScrollPane

--- src/com/iesvirgendelcarmen/gui/teoria/PanelTexto.java
+++ src/com/iesvirgendelcarmen/gui/teoria/PanelTexto.java
@@ -1,6 +1,14 @@
 package com.iesvirgendelcarmen.gui.teoria;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.ScrollPane;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingUtilities;
 
 public class PanelTexto {
@@ -8,7 +16,7 @@ public class PanelTexto {
 
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				JFrame frame = new InterfazCalculadora();
+				JFrame frame = new InterfazPanelTexto();
 				frame.setVisible(true);
 
 			}
@@ -20,14 +28,62 @@ public class PanelTexto {
 
 class InterfazPanelTexto extends JFrame {
 	
+	JLabel labelTitulo;
+	private JPanel miPanel;
+	JTextArea areaTexto;
+	String texto;
 	
+	JScrollPane scrollPane;
+
 	public InterfazPanelTexto() {
 		crearVentana();
+		iniciarComponentes();
+
+	}
+
+	private void iniciarComponentes() {
+
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(5, 10, 380, 150);
 		
+		miPanel = new JPanel();
+		miPanel.setLayout(null);
+
+		labelTitulo = new JLabel();
+		labelTitulo.setText("CONTENEDOR JCROLLPANE");
+		labelTitulo.setBounds(20, 20, 180, 23);
+		
+
+		texto = "El programa proporciona el área de texto como argumento al \nconstructor "
+				+ "del JScrollPane. Esto establece el área de texto \ncomo el cliente del panel "
+				+ "desplazable. El panel desplazable  \nmaneja todo esto: crear las barras de "
+				+ "desplazamiento cuando son necesarias, redibujar el cliente cuando  \nel usuario "
+				+ "se mueve sobre él, etc. Observa que el código de \nejemplo selecciona el tamaño "
+				+ "preferido del contenedor del \npanel desplazable. Una alternativa sería seleccionar "
+				+ "el \ntamaño preferido del propio panel desplazable. De cualquier \nmodo, se está "
+				+ "limitando el tamaño del panel desplazable. \nEsto es necesario porque el tamaño "
+				+ "preferido de un panel \ndesplazable es ser tan grande como pueda. Por defecto, "
+				+ "un \npanel desplazable intenta redimensionarse para que su...";
+
+		areaTexto = new JTextArea();
+		areaTexto.setText(texto);
+		areaTexto.setBounds(35, 60, 340, 1000);
+			
+		miPanel.setPreferredSize(new Dimension(350, 250));
+		scrollPane.setViewportView(miPanel);
+		
+		miPanel.add(labelTitulo);
+		miPanel.add(areaTexto);
+		add(scrollPane);
 	}
 
 	private void crearVentana() {
-
+		setTitle("Panel con JScrollPane");
+		setSize(420, 250);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+
+
 	}
 }
